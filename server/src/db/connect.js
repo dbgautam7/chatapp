13c599ae722b66c175580ed4ac321eb82911dd1f
D B Gautam <gautamdb28@gmail.com>
2023-03-10 22:03:35 +0545

    basic socket is setup on server side

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 module.exports = connect=async()=>{
     try{
-      //database = takeaway
-        await mongoose.connect('mongodb://127.0.0.1:27017/takeaway', {useNewUrlParser: true, useUnifiedTopology: true});
+        await mongoose.connect('mongodb://127.0.0.1:27017/chatapp', {useNewUrlParser: true, useUnifiedTopology: true});
         console.log("connected to mongodb");
     }catch(error){
         console.error(error);
--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,51 @@
 const express = require('express')
 const cors = require('cors')
-const connect=require("./db/connect")
-
 const app = express()
-
 app.use(express.json())
 app.use(cors())
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
+const io = new Server(server,{
+  cors: {
+      origin: "*"
+  },
+});
+
+
+
+const connect=require("./db/connect")
 connect()
 
 const hostname = '127.0.0.1';
 const port = 3000;
-app.listen(port, hostname, () => {
+
+io.on('connection', (socket) => {
+  console.log("socket is connected")
+});
+
+server.listen(port, hostname, () => {
     console.log(`Server running on port at http://${hostname}:${port}/`);
-  });
+});
+
+
+// let interval;
+
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 1000);
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//     clearInterval(interval);
+//   });
+// });
+
+// const getApiAndEmit = socket => {
+//   const response = new Date();
+//   // Emitting a new message. Will be consumed by the client
+//   socket.emit("FromAPI", response);
+// };
