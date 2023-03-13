f9830fac374b7e202349033cb395b87ce3773300
D B Gautam <gautamdb28@gmail.com>
2023-03-13 09:43:15 +0545

    login tested successfully from postman for server

---
+++ server/src/controllers/userLoginControllers.js
@@ -0,0 +1,35 @@
+const Users = require("../models/Users");
+const bcrypt = require("bcrypt")
+
+
+const Login=async (req, res) => {
+    try {
+        const user = await Users.findOne({ phoneOrEmail: req.body.phoneOrEmail }).lean();
+        console.log(user,"&&")
+        if (user) {
+          const {password } = user;
+          const isMatched = bcrypt.compareSync(req.body.password, password);
+          console.log(isMatched,req.body.password,"bool")
+          if (isMatched) {
+            res.status(200).json({
+              msg: "Logged in successfully",
+            });
+          } else {
+            res.status(401).json({
+              errorMsg: "Invalid credentials",
+            });
+          }
+        } else {
+          res.status(404).json({
+            errorMsg: "User not found",
+          });
+        }
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          errorMsg: "Internal server error",
+        });
+      }
+    };
+  
+  exports.Login=Login
--- server/src/routes/userRouters.js
+++ server/src/routes/userRouters.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const app = Router();
-const userControllers = require("../controllers/userControllers")
+const userRegisterControllers = require("../controllers/userRegisterControllers")
+const userLoginControllers = require("../controllers/userLoginControllers")
 
-app.post('/register',userControllers.Register)
+app.post('/register',userRegisterControllers.Register)
+
+app.post('/login',userLoginControllers.Login)
 
 module.exports = app;
